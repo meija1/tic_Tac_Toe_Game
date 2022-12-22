433af7b37daa7839869004f590bce506e095d2e1
meija1 <meija911@gmail.com>
2022-12-22 16:24:20 +0000

    Fix button and addgame area for website

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+
+function showGame () {
+    let gameBoard = document.getElementById('game-board');
+    gameBoard.style.display = "block";
+}
+
+function gameArea() {
+
+}
+
+function checkWinner() {
+
+}
