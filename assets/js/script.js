436865059795c4f83057e07cfdcbfef4db31a757
meija1 <meija911@gmail.com>
2022-12-26 12:56:12 +0000

    Clear up some code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
+const pos = document.querySelectorAll('data-cell');
 const winningPos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
-runGame();
+
 function showGame(){
     document.getElementById('game-board').style.display = 'block';
 }
 
 function runGame(){
-    let pos1 = document.querySelectorAll('[data-cell]');
-    
+    pos.addEventListener('click', (event) => {
+        const {target} = event;
+        target.textContent = 'X';
+    });
+    return pos;
 }
 
