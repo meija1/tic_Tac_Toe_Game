bd890707efdfe10e5074199bbf6efc089ffd29ea
meija1 <meija911@gmail.com>
2022-12-26 12:20:14 +0000

    Add simple function code and style for X and O elements

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const winningPos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+runGame();
+function showGame(){
+    document.getElementById('game-board').style.display = 'block';
+}
+
+function runGame(){
+    let pos1 = document.querySelectorAll('[data-cell]');
+    
+}
+
