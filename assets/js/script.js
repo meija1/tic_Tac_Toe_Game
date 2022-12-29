76a141c1a5a71cfeb94f91a2e1f6db89a76ea1b5
meija1 <meija911@gmail.com>
2022-12-29 10:44:15 +0000

    Add game mode function and make a elements clickable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,41 @@
 const pos = document.querySelectorAll('[data-cell]');
 const gameBoard = document.getElementById('backboard');
-runGame();
 const winningPos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+const currentPlayer = 'X';
+gameMode()
+
 
 function showGame(){
     document.getElementById('game-board').style.display = 'block';
 }
 
-function runGame(){
+function gameMode(){
+    let gameMode1 = document.getElementById('gMode1');
+    let gameMode2 = document.getElementById('gMode2');
+    if(gameMode1){
+        return vsCpu();
+    }else if(gameMode2) {
+        return vsPlayer();
+    }
+}
+
+function vsPlayer(){
     for(let i = 0; i < pos.length; i++){
+        if(currentPlayerTurn){
         pos[i].addEventListener('click', function(){
             pos[i].classList.toggle('x');
-            playerXturn = false;
+            currentPlayer = 'O';
         });
+    }else {
+        pos[i].classList.toggle('o');
+    }
     }
 }
 
 function vsCpu() {
+    
 }
 
-function vsPlayer() {
-}
+
+    
 
