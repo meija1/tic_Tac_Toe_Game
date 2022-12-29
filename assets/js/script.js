0c76742c5ee5acb488e05b0a2f687987c097430c
meija1 <meija911@gmail.com>
2022-12-29 14:24:13 +0000

    Fix and clear up some code

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const pos = document.querySelectorAll('[data-cell]');
 const gameBoard = document.getElementById('backboard');
 const winningPos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
 let currentPlayer = 'X';
+let = playerTurn = true;
 
 
 function showGame(){
@@ -9,45 +10,50 @@ function showGame(){
 }
 
 function gameMode(){
-    let vsPlayerMode = document.getElementById('gMode1');
-    let vsCpuMode = document.getElementById('gMode2');
+    let gameActive = true;
+    let vsPlayerMode = document.getElementById('gMode2');
+    let vsCpuMode = document.getElementById('gMode1');
     vsPlayerMode.addEventListener('click', function(){
-        vsPlayerMode.classList.add('active');
+        if(gameActive){
+        vsPlayerMode.classList.toggle('active');
+        vsCpuMode.classList.remove('active');
         vsPlayer();
-    })
+        }
+    });
     vsCpuMode.addEventListener('click', function(){
+        gameActive = false;
+        if(!gameActive){
         vsCpuMode.classList.add('active');
         vsPlayerMode.classList.remove('active');
-        vsCpu()
-    })
+        vsCpu();
+    }
+    });
 }
 
 function vsPlayer(){
     for(let i = 0; i < pos.length; i++){
-        if(currentPlayerTurn){
+        if(playerTurn === true){
         pos[i].addEventListener('click', function(){
-            pos[i].classList.toggle('x');
+            pos[i].classList.remove('o');
+            pos[i].classList.add('x');
+            playerTurn = false;
         });
-    }else {
-        pos[i].classList.toggle('o');
+    }else if(playerTurn === false) {
+        pos[i].addEventListener('click', function(){
+            pos[i].classList.remove('x');
+            pos[i].classList.add('o');
+    });
     }
-    }
+}
 }
 
 function vsCpu() {
     for(let i = 0; i < pos.length; i++){
-        if(currentPlayerTurn){
+        if(playerTurn){
         pos[i].addEventListener('click', function(){
-            pos[i].classList.toggle('o');
+            pos[i].classList.remove('x');
+            pos[i].classList.add('o');
         });
     }
 }
 }
-
-function currentPlayerTurn() {
-    currentPlayer = currentPlayer =='X' ? currentPlayer = 'O' : currentPlayer = 'X';
-}
-
-
-    
-
