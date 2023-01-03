aea77d4cc3415e40b36f6f3fae7cfd3c63cfd043
meija1 <meija911@gmail.com>
2023-01-03 21:00:05 +0000

    Fix code for elements who show outside gameboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,33 @@
-let pos = document.querySelectorAll('[data-cell]');
-const gameBoard = document.getElementById('backboard');
+let pos = document.querySelectorAll('data-cell');
 const winningPos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]];
+const vsPlayerMode = document.getElementById('gMode2');
+const vsCpuMode = document.getElementById('gMode1');
 let currentPlayer = true;
 let xPlayer = [];
 let oPlayer = [];
-
 
 function showGame(){
     document.getElementById('game-board').style.display = 'block';
 }
 
-function gameMode(){
-    const vsPlayerMode = document.getElementById('gMode2');
-    const vsCpuMode = document.getElementById('gMode1');
-    vsPlayerMode.addEventListener('click', function(){
-        vsPlayerMode.classList.toggle('active');
-        vsCpuMode.classList.remove('active');
-        vsPlayer();
-    });
-    vsCpuMode.addEventListener('click', function(){
-        vsCpuMode.classList.add('active');
-        vsPlayerMode.classList.remove('active');
-        vsCpu();
-    
-    });
-}
+vsPlayerMode.addEventListener('click', vsPlayer);
+vsCpuMode.addEventListener('click', vsCpu);
 
 function vsPlayer(){
     pos = document.addEventListener('click', event => {
         pos = event.target;
+        let posTrue = pos.hasAttribute('data-cell');
+        if(posTrue){
+            let cell = pos.dataset.value;
+            currentPlayer === true ? xPlayer.push(cell) : oPlayer.push(cell);
         pos.classList.add(currentPlayer ? 'x' : 'o');
         currentPlayer = !currentPlayer;
-        
-    });    
-}
+        }
+    });  
+
+}  
+
 
 function vsCpu() {
-    for(let i = 0; i < pos.length; i++){
-        if(currentPlayer){
-        pos[i].addEventListener('click', function(){
-            pos[i].classList.remove('x');
-            pos[i].classList.add('o');
-        });
-    }
-}
+    
 }
