ae37fd3726bdc587d547792c13f597469b13322b
meija1 <meija911@gmail.com>
2023-01-06 13:38:38 +0000

    Add restart game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 let pos = Array.from(document.querySelectorAll('data-cell'));
 const vsPlayerMode = document.getElementById('gMode2');
 const vsCpuMode = document.getElementById('gMode1');
+let winPositions = [
+    ['0', '1', '2'],
+    ['3', '4', '5'],
+    ['6', '7', '8'],
+    ['0', '3', '6'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['0', '4', '8'],
+    ['2', '4', '6']
+];
 let currentPlayer = true;
 let xPlayer = [];
 let oPlayer = [];
@@ -14,22 +24,21 @@ vsCpuMode.addEventListener('click', vsCpu);
 
 function vsPlayer() {
     document.addEventListener('click', event => {
-    pos = event.target;
-    let posTrue = pos.hasAttribute('data-cell');
+        pos = event.target;
+        let posTrue = pos.hasAttribute('data-cell');
 
         if (posTrue) {
-            
             pos.style.pointerEvents = "none";
             let cell = pos.getAttribute('data-cell');
             currentPlayer === true ? xPlayer.push(cell) : oPlayer.push(cell);
             pos.classList.add(currentPlayer ? 'x' : 'o');
             currentPlayer = !currentPlayer;
 
-          if(xPlayer.length >= 3) {
-            checkWinner()
-         }
+            if (xPlayer.length >= 3) {
+                checkWinner()
+            }
         }
-});
+    });
 
 }
 
@@ -39,16 +48,15 @@ function vsCpu() {
 }
 
 function checkWinner() {
-    const winPositions = [['0','1','2'],['3','4','5'],['6','7','8'],['0','3','6'],['1','4','7'],['2','5','8'],['0','4','8'],['2','4','6']];
     winPositions.forEach(winPos => {
         const xWon = winPos.every(pos => xPlayer.includes(pos))
         const oWon = winPos.every(pos => oPlayer.includes(pos))
-        
-        if(xWon || oWon){
+
+        if (xWon || oWon) {
             document.getElementById('startAgain').style.display = 'block';
             document.getElementById('text').textContent = xWon ? "X Won" : "O Won";
             restartGame();
-        } else if(xPlayer.length >= 5) {
+        } else if (xPlayer.length >= 5) {
             document.getElementById('startAgain').style.display = 'block';
             document.getElementById('text').textContent = "It's a Draw!"
             restartGame();
@@ -57,9 +65,10 @@ function checkWinner() {
 }
 
 function restartGame() {
-    document.getElementById('restart').addEventListener('click', function(){
+    document.getElementById('restart').addEventListener('click', function () {
         document.getElementById('startAgain').style.display = 'none';
-    });
     xPlayer = [];
     oPlayer = [];
+    });
+    
 }
